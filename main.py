9aa9663a777bdc441f12aaabd5f7338710ac64c0
unknown <suket_1111@icloud.com>
2020-07-01 19:30:00 +0900

    test

--- main.py
+++ main.py
@@ -57,9 +57,12 @@ def callback():
 # MessageEvent
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    text = event.message.text
+    x = symbols('x')
+    y = sympify(text)
 	line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text='「' + event.message.text + '」って何？')
+        TextSendMessage(text=y)
      )
 
 

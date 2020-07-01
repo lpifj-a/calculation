3a404c84d03dd009e2217254e99c35d307540b34
unknown <suket_1111@icloud.com>
2020-07-01 21:40:04 +0900

    test

--- main.py
+++ main.py
@@ -55,18 +55,19 @@ def callback():
     return 'OK'
 
 # MessageEvent
-@handler.add(MessageEvent, message=ImageMessage)
+@handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     text = event.message.text
     x = symbols('x')
     y = sympify(text)
     g = plotting.plot(y)
     g.save("a.png")
-    image_message = ImageSendMessage(
-        url=f"https://calculation-sympy.herokuapp.com/a.png",
+    url = request.url_root + '/static/a.png'
+    app.logger.info("url=" + url)
+    line_bot_api.reply_message(
+        event.reply_token,
+        ImageSendMessage(url, url)
     )
-
-    line_bot_api.reply_message(event.reply_token, image_message)
 
 @handler.add(FollowEvent)
 def handle_follow(event):
